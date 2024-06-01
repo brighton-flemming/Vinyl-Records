0e0994cb6b85a9dc897e90b0c23935eb89e2e1ea
Brighton <brighton.flemming@student.moringaschool.com>
2024-06-01 23:32:55 +0200

    Install of a few packages

--- src/Controller/RecordController.php
+++ src/Controller/RecordController.php
@@ -6,13 +6,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+
+
 class RecordController extends AbstractController
 {
     #[Route('/record', name: 'app_record')]
     public function index(): Response
     {
-        return new Response('
-        <strong>This is the new vinyl records app</strong>
-        ');
+        $songCount = 80;
+
+        $myAlbum = [
+            'name' => "Inner love",
+            'artist' => "MAGIC!",
+            'likes' => 21387400,
+            'favouriteTrack' => "Truly Happy"
+        ];
+
+        return $this-> render('record/homepage.html.twig', [
+            'numberOfSongs' => $songCount,
+            'myAlbum' => $myAlbum,
+        ]);
     }
 }
